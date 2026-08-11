7b9410726ad6b05279ed8bec4f92bd71b267932f
Maksim Zholudev <maximzms@gmail.com>
2026-08-11 09:59:31 +0300

    Tail

---
+++ 06_qs_tail/qs_tail.py
@@ -0,0 +1,46 @@
+def ArrayScan(array: list[int], lo: int, up: int, rf: int) -> int:
+    assert lo <= up
+    assert lo >= 0
+    assert up < len(array)
+    while True:
+        while array[lo] < array[rf]:
+            lo += 1
+        while array[up] > array[rf]:
+            up -= 1
+        if lo == up - 1 and array[lo] > array[up]:
+            array[lo], array[up] = array[up], array[lo]
+            return None
+        if lo == up or (lo == up - 1 and array[lo] < array[up]):
+            return rf
+        array[lo], array[up] = array[up], array[lo]
+        if rf == lo:
+            rf = up
+            continue
+        if rf == up:
+            rf = lo
+
+
+def ArrayChunk(array: list[int], left: int, right: int) -> int:
+    assert left <= right
+    assert left >= 0
+    assert right < len(array)
+    if left == right:
+        return left
+    while True:
+        rf = ArrayScan(array, left, right, (left + right) // 2)
+        if rf is not None:
+            return rf
+
+def QuickSortTailOptimization(array: list[int], left: int, right: int):
+    if left >= right:
+        return
+    middle = ArrayChunk(array, left, right)
+    # Recursion on smaller part, tail recursion on larger part
+    if middle - left < right - middle:
+        QuickSortTailOptimization(array, left, middle - 1)
+        QuickSortTailOptimization(array, middle + 1, right)
+        return
+    QuickSortTailOptimization(array, middle + 1, right)
+    QuickSortTailOptimization(array, left, middle - 1)
+
+
---
+++ 06_qs_tail/test_qs_tail.py
@@ -0,0 +1,34 @@
+from qs_tail import *
+
+def test_hoar_sort():
+    a = []
+    QuickSortTailOptimization(a, 0, 0)
+    assert a == []
+
+    a = [1]
+    QuickSortTailOptimization(a, 0, 0)
+    assert a == [1]
+
+    a = [1, 2]
+    QuickSortTailOptimization(a, 0, 1)
+    assert a == [1, 2]
+
+    a = [2, 1]
+    QuickSortTailOptimization(a, 0, 1)
+    assert a == [1, 2]
+
+    for a in [
+        [1, 2, 3],
+        [1, 3, 2],
+        [2, 1, 3],
+        [2, 3, 1],
+        [3, 1, 2],
+        [3, 2, 1],
+    ]:
+        QuickSortTailOptimization(a, 0, 2)
+        assert a == [1, 2, 3]
+
+    a = [7, 5, 6, 4, 9, 1, 2, 8, 3]
+    QuickSortTailOptimization(a, 0, 8)
+    assert a == [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
